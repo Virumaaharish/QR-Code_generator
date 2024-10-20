f520bf5e21b9713c76a21d10a60b786b2468da18
deepika3095 <151625159+deepika3095@users.noreply.github.com>
2024-10-20 19:15:39 +0530

    Update QR_code_generator.py

--- QR_code_generator.py
+++ QR_code_generator.py
@@ -1,20 +1,20 @@
 import qrcode
 
-# Enter url of any website here.
+# Enter the URL of any website here
 input_URL = "https://www.google.com/"
 
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=15,
-    border=4,
+    box_size=20,  # Set the box size to 20pt
+    border=0,  # Remove the border by setting it to 0
 )
 
 qr.add_data(input_URL)
 qr.make(fit=True)
 
-# convert into image
+# Convert into an image with red fill color and white background
 img = qr.make_image(fill_color="red", back_color="white")
 img.save("url_qrcode.png")
 
-print(qr.data_list)
+print(qr.data_list)  # Print the data list used for generating the QR code
